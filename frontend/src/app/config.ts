fbb2853cc3dbebdbac8c406710a1bfb0897fed46
Matias Aravena <matias.aravena.g@gmail.com>
2026-05-30 22:55:19 +0200

    rename

--- frontend/src/app/config.ts
+++ frontend/src/app/config.ts
@@ -3,7 +3,7 @@ import { provideRouter, withInMemoryScrolling } from '@angular/router';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
 
-import { routes } from './app.routes';
+import { routes } from './routes';
 import { adminAuthInterceptor } from './shared/admin/admin-auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
